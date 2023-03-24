bfb87efe150caf4a73298142d8877aebb41f85bf
Anton Khaynovskiy <a.khaynovskiy@skyeng.ru>
2023-03-24 15:09:03 +0100

    make app standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
+  standalone: true,
+  imports: []
 })
 export class AppComponent {
   title = 'star-wars';
